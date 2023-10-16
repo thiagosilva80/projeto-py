d1cb8355f2bfc36c8d8de27cefa560421574724b
thiagosilva80l <teteusabri08@gmail.com>
2023-10-16 16:38:55 -0300

    first commit

---
+++ atv1.py/atv11.py
@@ -0,0 +1,6 @@
+nota1 = float(input('insira a primeira nota'))
+nota2 = float (input('insira a segunda nota'))
+nota3 = float (input('insira a terceira nota '))
+nota4 = float (input(' insira a quarta nota'))
+
+media = ( nota1 + nota2 + nota3 + nota4) / 4 print(f'a sua media e {media}')
